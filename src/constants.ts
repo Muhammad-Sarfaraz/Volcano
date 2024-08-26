7ca6136263e0f6131dd75d5cb0fe310408a32925
Sarfaraz Muhammad Sajib <id.sent.me@gmail.com>
2024-08-26 17:14:58 +0600

    fix : rule

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,6 @@
 export const GOOGLE_API_ENDPOINT =
   "https://generativelanguage.googleapis.com/v1/models/gemini-pro:generateContent";
-export const GOOGLE_API_KEY = "AIzaSyAvCz8Dvku0xyQ3JwlAItEuKAOqHdDZLho";
+
 
 export const OPEN_API_ENDPOINT = "https://api.openai.com/v1/chat/completions";
-export const OPEN_API_KEY =
-  "sk-bm9ds3DwzHRpAUTfsq20PezPNb5WbhK6smd2KLLyWoT3BlbkFJlMHnQtu6qlomhocHUiNKCS2pJ7VuhbKAp1_a7CNMUA";
+
